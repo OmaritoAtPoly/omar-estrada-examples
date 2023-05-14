e2a124a8aa4a0910bfebd7037948ba07166bbfee
Omar Estrada Diaz <omarito4857@gmail.com>
2023-05-14 12:11:24 +0300

    adding dates in the table to show

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,47 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import './App.css';
-import Papa, { ParseResult } from 'papaparse';
+import Papa, {ParseResult} from 'papaparse';
 import shortid from 'shortid';
+import {biggestDate, dateCalculator, smallestDate} from './utiles/functions';
 
 type Data = {
-  'Employee ID #1': string
-  'Employee ID #2': string
-  'Project ID': string
-  'Days worked': string
+  'EmpID': string
+  'ProjectID': string
+  'DateFrom': string
+  'DateTo': string
 }
 
-type Values = {
-  data: Data[]
+type headerKeys = {
+  'Employee ID #1': string,
+  'Employee ID #2': string,
+  'Project ID': string,
+  'Days worked': number,
 }
 
-const headerKeys = ['Employee ID #1', 'Employee ID #2', 'Project ID', 'Days worked Together']
+const header = ['Employee ID #1', 'Employee ID #2', 'Project ID', 'Days worked Together']
 
 function App() {
   const [file, setFile] = useState<File>();
-  const [values, setValues] = React.useState<Values | undefined>()
+  const [values, setValues] = React.useState<Data[]>()
+  let commonProjects: Data[][] = [];
+  let finalValues: headerKeys[] = [];
 
-  const getCSV = useCallback (() => {
+  const getCSV = useCallback(() => {
     if (file) {
       Papa.parse(file, {
-        header: true,
         download: true,
         skipEmptyLines: true,
         delimiter: ",",
-        complete: (results: ParseResult<Data>) => {
-          setValues(results)
+        complete: (results: ParseResult<string[]>) => {
+
+          const objMade = results.data.map((a) => ({
+            'EmpID': a[0],
+            'ProjectID': a[1],
+            'DateFrom': a[2],
+            'DateTo': a[3],
+          }))
+
+          setValues(objMade)
         },
       })
     }
@@ -47,7 +60,26 @@ function App() {
 
   useEffect(() => {
     getCSV()
-  }, [file, getCSV])
+  }, [getCSV])
+
+  if (values) {
+    for (let i = 0; i < values.length; i++) {
+      for (let j = i; j < values.length; j++) {
+        if (values[i].EmpID !== values[j].EmpID && values[i].ProjectID === values[j].ProjectID) {
+          commonProjects = [...commonProjects, [values[i], values[j]]]
+        }
+      }
+    }
+  }
+
+  finalValues = commonProjects.map((a, i) => ({
+
+    "Employee ID #1": a[0].EmpID,
+    "Employee ID #2": a[1].EmpID,
+    "Project ID": a[0].ProjectID,
+    "Days worked": Number(dateCalculator(biggestDate(a[0].DateFrom, a[1].DateFrom), smallestDate(a[0].DateTo, a[1].DateTo)))
+
+  }))
 
   return (
     <div className="App">
@@ -61,24 +93,23 @@ function App() {
         </form>
 
         <table>
-        <thead>
-          <tr key={"header"} >
-            {values?.data && headerKeys.map((key) => (
-              <th key={shortid.generate()}>{key}</th>
-            ))}
-          </tr>
-        </thead>
-
-        <tbody>
-          {values?.data.map((item) => (
-            <tr key={shortid.generate()}>
-              {Object.values(item).map((val) => (
-                <td key={shortid.generate()}>{val}</td>
+          <thead>
+            <tr key={"header"} >
+              {values && header.map((key) => (
+                <th key={shortid.generate()}>{key}</th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {finalValues && finalValues.map((item) => (
+              <tr key={shortid.generate()}>
+                {Object.values(item).map((val) => (
+                  <td key={shortid.generate()}>{val}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
 
       </header>
     </div>
---
+++ src/utiles/functions.ts
@@ -0,0 +1,40 @@
+export const dateCalculator = (DateFrom: string, DateTo: string) => {
+
+	const date1 = new Date(DateFrom);
+	let date2 = new Date(DateTo);
+
+	if (DateTo === 'NULL') {
+		date2 = new Date();
+	}
+	// To calculate the time difference of two dates
+	const DifferenceInTime = date2.getTime() - date1.getTime();
+
+	// To calculate the no. of days between two dates
+	const DifferenceInDays = DifferenceInTime / (1000 * 3600 * 24);
+
+	return DifferenceInDays.toFixed();
+}
+
+export const biggestDate = (firstDate: string, secondDate: string) => {
+	const date1 = new Date(firstDate).getTime();
+	let date2 = new Date(secondDate).getTime();
+
+	if (secondDate === 'NULL') {
+		date2 = new Date().getTime();
+	}
+
+	if (date1 > date2) return firstDate;
+	return secondDate;
+}
+
+export const smallestDate = (firstDate: string, secondDate: string) => {
+	const date1 = new Date(firstDate).getTime();
+	let date2 = new Date(secondDate).getTime();
+
+	if (secondDate === 'NULL') {
+		date2 = new Date().getTime();
+	}
+
+	if (date1 < date2) return firstDate;
+	return secondDate;
+}
