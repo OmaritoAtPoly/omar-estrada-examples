55da03da8186a958b23ad46b86909ab9e9f7fc57
Omar Estrada Diaz <omarito4857@gmail.com>
2023-05-29 01:26:28 +0300

    removing unused var in ImageSlider file

--- src/components/container/ImageSlider.tsx
+++ src/components/container/ImageSlider.tsx
@@ -1,4 +1,4 @@
-import {useCallback, useRef, useState} from 'react';
+import {useCallback, useState} from 'react';
 import {useCustomHook} from '../../utils/functions';
 import {Slider} from '../visual/Slider';
 
